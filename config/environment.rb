302e7038c8de5e0c5e81bf1ea6e4293dd9b140f4
Serah-Njenga <serah35.nn@gmail.com>
2023-05-30 21:22:31 +0300

    commit

--- config/environment.rb
+++ config/environment.rb
@@ -4,3 +4,11 @@ require 'bundler/setup'
 Bundler.require(:default, ENV["RACK_ENV"])
 
 require_relative "../app/models/artist"
+
+
+ActiveRecord::Base.establish_connection(
+  :adapter => "sqlite3",
+  :database => "db/development.sqlite3"
+) 
+
+require_relative "../app/models/artist"
